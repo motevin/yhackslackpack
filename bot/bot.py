64a2710996135011fdbdb56b83c32fe5739269af
Tevin Otieno <totieno@placester.com>
2015-11-07 18:59:32 -0500

    fucked rabbitmq code

--- bot/bot.py
+++ bot/bot.py
@@ -1,8 +1,82 @@
 ﻿import time
 import ConfigParser
+import pika
+import imp
+import json
 from slackclient import SlackClient
 
+PROJECT_ROOT = "/Users/tevin/dev/yhackslackpack"
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+sc = None
+slack_channel = None
+
+def wait_for_response(queue):
+    channel.queue_declare(queue=queue)
+    print "Waiting for response from user " + queue
+    channel.basic_consume(callback, queue=queue, no_ack=True, consumer_tag=queue)
+    channel.start_consuming()
+
+def callback(ch, method, properties, body):
+    print "Bot Received %r" % (body,)
+    sc.rtm_send_message(slack_channel, body)
+
+#==========================
+# COMMUNICATE WITH SERVICES
+#=========================
+# Sends messages to the service with the format
+#
+# {
+#     "user":""
+#     "message":""
+# }
+#
+# It's up to the service to parse the message.
+# Bot will wait for a response to post back to slack
+
+def send_message_and_await_response(queue, message):
+    print "Service already running"
+    channel.queue_declare(queue=queue)
+    channel.basic_publish(exchange='',routing_key=queue,body=message)
+    print "Sending message to service for user " + queue
+    print "\nMessage: " + message
+    wait_for_response(queue)
+
+def start_service_and_await_response(service, user, message):
+    print "Starting service " + service
+    service_obj = get_service_module(service)
+    payload = {}
+    payload['user'] = user
+    payload['message'] = message
+    service_obj.main(json.dumps(payload))
+    wait_for_response(user)
+
+def get_service_module(service):
+    config = ConfigParser.ConfigParser()
+    config.read(PROJECT_ROOT + "/modules.ini")
+    service_path = PROJECT_ROOT + config.get('Modules', service)
+    return imp.load_source('module.name', service_path)
+
+# Process input from slack and either spin up the related service
+# or send the message as part of an ongoing exchange
+def process(user, message):
+    global channel
+    # If a queue exists for the user, send it on that queue
+    try:
+        channel.queue_declare(queue=user, passive=True)
+        send_message_and_await_response(user, message)
+
+    #Service is currently not running. We need to start it. Idk how to MQ
+    except pika.exceptions.ChannelClosed:
+        channel = connection.channel()
+        channel.queue_declare(queue=user)
+        service = "tinder" #assume the service name is the first thing in the message. Yes its janky. This is a fucking hackathon
+        message.strip(service)
+        start_service_and_await_response(service, user, message)
+
 def run(apikey):
+    global sc, slack_channel
     sc = SlackClient(apikey)
     if sc.rtm_connect():
         global generalID
@@ -14,16 +88,19 @@ def run(apikey):
                 if 'type' in response[0] and response[0]['type'] == 'message':
                     # direct messages start with D
                     if (response[0]['channel'][0] == 'D'):
-                        sc.rtm_send_message(response[0]['channel'], 'yo')
+                        slack_channel = response[0]['channel']
+                        process(response[0]['user'], response[0]['text'])
             time.sleep(1)
     else:
         print 'connection failed'
 
+
 def main():
     config = ConfigParser.ConfigParser()
-    config.read('../../credentials.ini')
+    config.read("/Users/tevin/dev/yhackslackpack/credentials.ini")
     token = config.get('Slack', 'apikey')
     run(token)
+
 
 if __name__ == '__main__':
     main()
--- integrations/tinder/tinder.py
+++ integrations/tinder/tinder.py
@@ -1,14 +1,56 @@
 import pynder
+import sys
 import ConfigParser
+import pika
+import json
 from pymongo import MongoClient
 
 session = None
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+def get_likes_remaining(user):
+    likes = str(session.likes_remaining)
+    print likes
+    send_message_and_await_response(user, likes)
+
+#
+# MESSAGE PASSING METHODS
+#
+
+def wait_for_response(queue):
+    print "Waiting for response from slack " + queue
+    channel.basic_consume(callback, queue=queue, no_ack=True, consumer_tag=queue)
+    channel.start_consuming()
+
+# This is what you do when you get back a response
+def callback(ch, method, properties, body):
+    print "Tinder Received %r" % (body,)
+    parse(body)
+
+def send_message_and_await_response(queue, message):
+    channel.queue_declare(queue=queue)
+    channel.basic_publish(exchange='',routing_key=queue,body=message)
+    print "Sending message to service for user " + queue
+    print "\nMessage: " + message
+    wait_for_response(queue)
+
+def send_message_and_exit(queue, message):
+    channel.queue_declare(queue=queue)
+    channel.basic_publish(exchange='',routing_key=queue,body=message)
+    print "Sending message to service for user " + queue
+    print "\nMessage: " + message
+    connection.close()
+
+
+##############################################
 
 # Connects to mongo and returns a MongoClient
 def connect_to_mongo():
 
     credentials = ConfigParser.ConfigParser()
-    credentials.read("../../credentials.ini")
+    credentials.read("/Users/tevin/dev/yhackslackpack/credentials.ini")
 
     host = credentials.get("Mongo", "connection")
     user = credentials.get("Mongo", "user")
@@ -19,24 +61,42 @@ def connect_to_mongo():
     client = MongoClient(connection_url)
     return client[db]
 
-# Get sesion credentials for user in mongo
+# Get session credentials for user in mongo
 # If the user/required details do not exist in mongo, it queries the bot for them
 def get_tinder_creds(user):
     db = connect_to_mongo()
+    print user
     user_tinder_data = db.users.find_one({"name": user}, {"tinder": 1})
-    access_token = user_tinder_data['tinder']['access_token']
-    fbid = user_tinder_data['tinder']['fbid']
-    print access_token + "\n" + fbid
-    return pynder.Session(fbid, access_token)
+    print user_tinder_data
+    if user_tinder_data:
+        access_token = user_tinder_data['tinder']['access_token']
+        fbid = user_tinder_data['tinder']['fbid']
+        print access_token + "\n" + fbid
+        return pynder.Session(fbid, access_token)
+    else:
+        quit()
 
+def get_user_from_json(json_obj):
+    return json.loads(json_obj)['user']
+
+def get_message_from_json(json_obj):
+    return json.loads(json_obj)['message']
+
+def parse(user, message):
+    print message
+    if "likes" in message:
+        get_likes_remaining(user)
+    else:
+        send_message_and_exit(user, message)
 # Creates a global Tinder session for the specified user and executes the given query
-def main(user):
+def main(args):
     global session
+    user = get_user_from_json(args)
     session = get_tinder_creds(user)
-    users = session.nearby_users()
-    for user in users[:5]:
-        print(user.get_photos())
-        print("\n")
+
+    message = get_user_from_json(args)
+    parse(user, message)
 
 if __name__ == '__main__':
-    main("U03FN2LLB")
+    print "Starting Tinder"
+    main(sys.argv)
